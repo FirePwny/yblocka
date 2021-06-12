b1e57d69c122cd1eb62a9d70a664ddd2119e2359
FirePwny <Spencer@DESKTOP-21MHLPT>
2021-06-12 02:04:17 -0500

    deez nuts

---
+++ src/main/java/net/mcreator/jojosblockyadventure/item/MysteriousArrowheadItem.java
@@ -0,0 +1,47 @@
+
+package net.mcreator.jojosblockyadventure.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.jojosblockyadventure.JojosblockyadventureModElements;
+
+@JojosblockyadventureModElements.ModElement.Tag
+public class MysteriousArrowheadItem extends JojosblockyadventureModElements.ModElement {
+	@ObjectHolder("jojosblockyadventure:mysterious_arrowhead")
+	public static final Item block = null;
+	public MysteriousArrowheadItem(JojosblockyadventureModElements instance) {
+		super(instance, 2);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MATERIALS).maxStackSize(16).isImmuneToFire().rarity(Rarity.UNCOMMON));
+			setRegistryName("mysterious_arrowhead");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+	}
+}
---
+++ src/main/java/net/mcreator/jojosblockyadventure/item/MysteriousChunkItem.java
@@ -0,0 +1,59 @@
+
+package net.mcreator.jojosblockyadventure.item;
+
+import net.minecraftforge.registries.ObjectHolder;
+
+import net.minecraft.world.World;
+import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.ITextComponent;
+import net.minecraft.item.Rarity;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.Item;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.block.BlockState;
+
+import net.mcreator.jojosblockyadventure.JojosblockyadventureModElements;
+
+import java.util.List;
+
+@JojosblockyadventureModElements.ModElement.Tag
+public class MysteriousChunkItem extends JojosblockyadventureModElements.ModElement {
+	@ObjectHolder("jojosblockyadventure:mysterious_chunk")
+	public static final Item block = null;
+	public MysteriousChunkItem(JojosblockyadventureModElements instance) {
+		super(instance, 1);
+	}
+
+	@Override
+	public void initElements() {
+		elements.items.add(() -> new ItemCustom());
+	}
+	public static class ItemCustom extends Item {
+		public ItemCustom() {
+			super(new Item.Properties().group(ItemGroup.MATERIALS).maxStackSize(64).rarity(Rarity.UNCOMMON));
+			setRegistryName("mysterious_chunk");
+		}
+
+		@Override
+		public int getItemEnchantability() {
+			return 0;
+		}
+
+		@Override
+		public int getUseDuration(ItemStack itemstack) {
+			return 0;
+		}
+
+		@Override
+		public float getDestroySpeed(ItemStack par1ItemStack, BlockState par2Block) {
+			return 1F;
+		}
+
+		@Override
+		public void addInformation(ItemStack itemstack, World world, List<ITextComponent> list, ITooltipFlag flag) {
+			super.addInformation(itemstack, world, list, flag);
+			list.add(new StringTextComponent("Something about this metal shakes you to your core..."));
+		}
+	}
+}
